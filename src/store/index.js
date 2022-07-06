fe7fddd483c49432d59079158c33b191f12c6bec
miyazoe.daisuke <hndfavodaismi@gmail.com>
2022-07-06 23:29:22 +0900

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
+import { parse } from '@vue/compiler-dom'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isloading: false
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem)
+      } else {
+        localStorage.getItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
